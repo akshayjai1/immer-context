898043f44b23c44283d8894ef3872343b269d9d3
Akshay Jain <akshay.jain@instarem.com>
2020-07-18 16:01:49 +0530

    adding libs

--- lib/IContext.d.ts
+++ lib/IContext.d.ts
@@ -1,3 +1,3 @@
-export declare const useIContext: () => {}[];
+export declare const useIContext: () => [any, any];
 export declare const IContext: (props: any) => JSX.Element;
 //# sourceMappingURL=IContext.d.ts.map
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
-export { IContext } from './IContext';
+export { IContext, useIContext } from './IContext';
 export { withIContext } from './withIContext';
 export { useImmer } from './useImmer';
 //# sourceMappingURL=index.d.ts.map
